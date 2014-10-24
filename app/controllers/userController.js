5921ba22077b931287945602573330cd1a629c70
narendrasoni1989 <narendrasoni2@gmail.com>
2014-10-24 13:44:51 +0800

    edit changes

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -3,31 +3,31 @@ var User = mongoose.model('User');
 var ObjectId = require('mongoose').Types.ObjectId;
 var fs = require('fs');
 exports.create = function(req,res){
-   var user = new User({
-      name:"Narendra",
-      email:"narendrasoni2@gmail.com",
-      username: "narendrasoni1989",
-      provider: "provider value",
-      hashed_password: "#$#$#$#$#$#"
-   });
-   
-   console.log("------ create---- is called.... ");
-   user.save(function(err){
-      if(err)
-         res.json({message: "Error occured while saving"});
-      else
-         res.json({message: "saved successfully"});
-   });
+    var user = new User({
+        name:"Narendra",
+        email:"narendrasoni2@gmail.com",
+        username: "narendrasoni1989",
+        provider: "provider value",
+        hashed_password: "#$#$#$#$#$#"
+    });
+
+    console.log("------ create---- is called.... ");
+    user.save(function(err){
+        if(err)
+            res.json({message: "Error occured while saving"});
+        else
+            res.json({message: "saved successfully"});
+    });
 };
 
 exports.getAll = function(req,res){
-	User.find(function(err,users){
-		if(err){
-			res.send(err);
-		}else{
-			res.json(users);
-		}
-	});
+    User.find(function(err,users){
+        if(err){
+            res.send(err);
+        }else{
+            res.json(users);
+        }
+    });
 };
 
 exports.getProfile = function(req,res){
@@ -38,7 +38,7 @@ exports.getProfile = function(req,res){
 };
 
 exports.saveProfile = function(req,res){
-   var id = new ObjectId(req.user._id);
+    var id = new ObjectId(req.user._id);
     User.findById(id,function(err,user){
         if(err){
             res.send(err);
@@ -89,28 +89,28 @@ exports.uploadProfilePic = function(req,res){
                     });
                 });
             };
-            });
         });
+    });
 };
 
 
 
 exports.get = function(req,res){
-	var id ;
-	try{
-	    req.params.user_id = '543b8b20bcd6970e06c6a893';
-	   id = new ObjectId(req.params.user_id);
-	   User.findById(id,function(err,user){
-		if(err){
-			res.send(err);
-		}
-		res.json(user);
-	});
-	}catch(e){
-	   
-	   res.send(404);
-	}
-	/*
+    var id ;
+    try{
+        req.params.user_id = '543b8b20bcd6970e06c6a893';
+        id = new ObjectId(req.params.user_id);
+        User.findById(id,function(err,user){
+            if(err){
+                res.send(err);
+            }
+            res.json(user);
+        });
+    }catch(e){
+
+        res.send(404);
+    }
+    /*
 	User.find({_id:id}).exec(function(err,user){
 		if(err || user.length ==0){
 			res.send("Error occured");
@@ -124,53 +124,53 @@ exports.get = function(req,res){
 		}
 		res.json(user);
 	});*/
-	
+
 };
 
 
 exports.update = function(req,res){
-	var id ;
-	try{
-	    req.params.user_id = '543b8b20bcd6970e06c6a893';
-	   id = new ObjectId(req.params.user_id);
-	   User.findById(id,function(err,user){
-		if(err){
-			res.send(err);
-		}
-		//user.username = req.body.username;
-		user.username = "min thu";
+    var id ;
+    try{
+        req.params.user_id = '543b8b20bcd6970e06c6a893';
+        id = new ObjectId(req.params.user_id);
+        User.findById(id,function(err,user){
+            if(err){
+                res.send(err);
+            }
+            //user.username = req.body.username;
+            user.username = "min thu";
 
-		//user.email = req.body.email;
-		user.email = "minthu@gmail.com";
+            //user.email = req.body.email;
+            user.email = "minthu@gmail.com";
 
-		//user.name = req.body.name;
-		user.name = "Narendra Soni";
+            //user.name = req.body.name;
+            user.name = "Narendra Soni";
 
-		user.save(function(err){
-			if(err)
-			    res.send(err);
-			res.json({message: "user  Updated successfully"});
-		});
-	});
-	}catch(e){
-	   res.send(404);
-	}
+            user.save(function(err){
+                if(err)
+                    res.send(err);
+                res.json({message: "user  Updated successfully"});
+            });
+        });
+    }catch(e){
+        res.send(404);
+    }
 };
 
 exports.delete = function(req,res){
-	var id ;
-	try{
-	   req.params.user_id = '543b8b20bcd6970e06c6a893';
-	   id = new ObjectId(req.params.user_id);
-	   User.remove({_id:id},function(err,user){
-	   	 console.log('checkpoint 4'); 
-		if(err){
-		   console.log("Error is:"+ err);
-		   res.send(err);
-		}
-	        res.json({message: "User deleted successfully"});
-	   });
-	}catch(e){
-	   res.send(404);
-	} 
+    var id ;
+    try{
+        req.params.user_id = '543b8b20bcd6970e06c6a893';
+        id = new ObjectId(req.params.user_id);
+        User.remove({_id:id},function(err,user){
+            console.log('checkpoint 4'); 
+            if(err){
+                console.log("Error is:"+ err);
+                res.send(err);
+            }
+            res.json({message: "User deleted successfully"});
+        });
+    }catch(e){
+        res.send(404);
+    } 
 };
--- routes/users.js
+++ routes/users.js
@@ -11,22 +11,22 @@ router.post('/:id/edit',isLoggedIn,selfLoggedIn,users.saveProfile);
 
 /* Middleware used for all requests */
 router.use(function(req,res,next){
-	//do loggin
-	console.log('Something is happening');	
-	next();
+    //do loggin
+    console.log('Something is happening');	
+    next();
 });
 
 
 /* GET users listing. */
 
 router.get('/', function(req, res) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 
 
 /* API */
 router.get('/api', function(req, res) {
-  res.json({ message: 'hooray! welcome to our api!' });	
+    res.json({ message: 'hooray! welcome to our api!' });	
 });
 
 
